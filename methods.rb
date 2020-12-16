b4ce4eeda8cee6ac88e136b4952843353b329e3a
YOUR_USERNAMEDarkoAU <GCAM022011@coderacademy.edu.au>
2020-12-17 10:40:02 +1100

    Added menu options, refactored code to use methods under menu options

--- methods.rb
+++ methods.rb
@@ -1,7 +1,66 @@
 require 'csv'
+
+def banner
+    puts "\n\n"   
+    puts "                                                                              ███████╗░█████╗░██████╗░  ██╗███╗░░░███╗██████╗░░█████╗░██████╗░████████╗"
+    puts "                                                                              ██╔════╝██╔══██╗██╔══██╗  ██║████╗░████║██╔══██╗██╔══██╗██╔══██╗╚══██╔══╝"
+    puts "                                                                              █████╗░░██║░░██║██████╦╝  ██║██╔████╔██║██████╔╝██║░░██║██████╔╝░░░██║░░░"
+    puts "                                                                              ██╔══╝░░██║░░██║██╔══██╗  ██║██║╚██╔╝██║██╔═══╝░██║░░██║██╔══██╗░░░██║░░░"
+    puts "                                                                              ██║░░░░░╚█████╔╝██████╦╝  ██║██║░╚═╝░██║██║░░░░░╚█████╔╝██║░░██║░░░██║░░░"
+    puts "                                                                              ╚═╝░░░░░░╚════╝░╚═════╝░  ╚═╝╚═╝░░░░░╚═╝╚═╝░░░░░░╚════╝░╚═╝░░╚═╝░░░╚═╝░░░"
+    puts "\n"
+    puts "                                                                              ░█████╗░░█████╗░██╗░░░░░░█████╗░██╗░░░██╗██╗░░░░░░█████╗░████████╗░█████╗░██████╗░"
+    puts "                                                                              ██╔══██╗██╔══██╗██║░░░░░██╔══██╗██║░░░██║██║░░░░░██╔══██╗╚══██╔══╝██╔══██╗██╔══██╗"
+    puts "                                                                              ██║░░╚═╝███████║██║░░░░░██║░░╚═╝██║░░░██║██║░░░░░███████║░░░██║░░░██║░░██║██████╔╝"
+    puts "                                                                              ██║░░██╗██╔══██║██║░░░░░██║░░██╗██║░░░██║██║░░░░░██╔══██║░░░██║░░░██║░░██║██╔══██╗"
+    puts "                                                                              ╚█████╔╝██║░░██║███████╗╚█████╔╝╚██████╔╝███████╗██║░░██║░░░██║░░░╚█████╔╝██║░░██║"
+    puts "                                                                              ░╚════╝░╚═╝░░╚═╝╚══════╝░╚════╝░░╚═════╝░╚══════╝╚═╝░░╚═╝░░░╚═╝░░░░╚════╝░╚═╝░░╚═╝"
+    puts "\n\n\n"
+    puts "-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------"
+end
+
+def menu_select
+    system "clear"
+    banner
+    puts "Press 1 for instructions on how to use this calculator"
+    puts "Press 2 to run the calculator"
+    puts "Press 3 to exit"
+    input = gets.chomp.to_i
+    return input
+end
+
+def quit 
+    puts "Are you sure you want to quit?"
+    response = gets.chomp
+    while response.downcase == "yes"
+        puts "Thank you for using our calculator and we hope to see you again soon!"
+        exit
+    begin
+        puts "invalid input"
+    rescue => exception
+end
+
+def how_to_use 
+    #system "clear"
+    banner
+    puts "   1. Enter your cargo volume. This will help us determine whether you will require to ship your goods as Less than Container Load (LCL) or as Full Container Load (FCL)"
+    puts "\n   2. Enter your load port. Supported ports are: "
+    pricing = CSV.parse(File.read("./docs/pricing.csv", headers: true))
+        pricing.each do |row| 
+                puts "\n                  "" - #{row[0]}"
+        end
+    puts "\n   3. The calculator will display your total price based on the provided cargo volume"
+    puts "\n\nDo you want to return to the main menu? (Type 'Y' and press enter)"
+    input = gets.chomp.downcase
+        if input == "y"
+        menu_select
+        else
+         puts "you have entered an invalid input"
+        end
+end
+
 def calculate_containers_number(volume)
     containers = {LCL: 0, twenty_foot: 0, fourty_foot: 0}
-
     if volume < 15
         containers[:LCL] += volume
         return containers
@@ -17,22 +76,20 @@ def calculate_containers_number(volume)
             end
         end  
     return containers
+    puts "   The most cost effective way to ship this cosningment is as #{containers}"
 end
 
 def origin_port(name, path)
     pricing = CSV.parse(File.read("./docs/pricing.csv", headers: true))
     row = pricing.find { |row| row.include? name} 
-    puts "the freight price per 20' container is US$#{row[1]}, the price freight price per 40' contaienr is US$#{row[2]}, and the LCL price per cubic meter is US$#{row[3]}"
+    return row
+    puts "the freight price per 20' container is US$#{row[1]}, the price freight price per 40' container is US$#{row[2]}, and the LCL price per cubic meter is US$#{row[3]}"
 end
 
 def calculate_mode(hash)
-    
+
 end
 
 def calculate_container_cost(hash)
 
 end 
-
-# def lcl_service (cargo_volume, "./docs/pricing.csv")
-#     lcl_freight_cost = cargo_volume * 
-# end
--- shipping_calculator.rb
+++ shipping_calculator.rb
@@ -1,32 +1,43 @@
 require 'csv'
+require 'tty-prompt'
 require_relative 'methods'
 
-puts "\n\n"
-puts "                         ███████╗░█████╗░██████╗░  ██╗███╗░░░███╗██████╗░░█████╗░██████╗░████████╗"
-puts "                         ██╔════╝██╔══██╗██╔══██╗  ██║████╗░████║██╔══██╗██╔══██╗██╔══██╗╚══██╔══╝"
-puts "                         █████╗░░██║░░██║██████╦╝  ██║██╔████╔██║██████╔╝██║░░██║██████╔╝░░░██║░░░"
-puts "                         ██╔══╝░░██║░░██║██╔══██╗  ██║██║╚██╔╝██║██╔═══╝░██║░░██║██╔══██╗░░░██║░░░"
-puts "                         ██║░░░░░╚█████╔╝██████╦╝  ██║██║░╚═╝░██║██║░░░░░╚█████╔╝██║░░██║░░░██║░░░"
-puts "                         ╚═╝░░░░░░╚════╝░╚═════╝░  ╚═╝╚═╝░░░░░╚═╝╚═╝░░░░░░╚════╝░╚═╝░░╚═╝░░░╚═╝░░░"
-puts "\n"
-puts "                         ░█████╗░░█████╗░██╗░░░░░░█████╗░██╗░░░██╗██╗░░░░░░█████╗░████████╗░█████╗░██████╗░"
-puts "                         ██╔══██╗██╔══██╗██║░░░░░██╔══██╗██║░░░██║██║░░░░░██╔══██╗╚══██╔══╝██╔══██╗██╔══██╗"
-puts "                         ██║░░╚═╝███████║██║░░░░░██║░░╚═╝██║░░░██║██║░░░░░███████║░░░██║░░░██║░░██║██████╔╝"
-puts "                         ██║░░██╗██╔══██║██║░░░░░██║░░██╗██║░░░██║██║░░░░░██╔══██║░░░██║░░░██║░░██║██╔══██╗"
-puts "                         ╚█████╔╝██║░░██║███████╗╚█████╔╝╚██████╔╝███████╗██║░░██║░░░██║░░░╚█████╔╝██║░░██║"
-puts "                         ░╚════╝░╚═╝░░╚═╝╚══════╝░╚════╝░░╚═════╝░╚══════╝╚═╝░░╚═╝░░░╚═╝░░░░╚════╝░╚═╝░░╚═╝"
+while true
+    case menu_select
+    when 1 
+        how_to_use
+    when 2
+        #system "clear"
+        banner
+        puts "Please provide total cargo volume"
+        cargo_volume = gets.chomp.to_f
+        calculate_containers_number(cargo_volume) 
+        puts "Now, please tell us your origin port"
+        load_port = gets.chomp.upcase
+        origin_port(load_port, "./docs/pricing.csv")
+    when 3
+        system "clear"
+        banner
+        quit
+    end
 
-puts "\n\nWelcome to the Melbourne Import FOB calculator"
-puts "This calculator will allow you to enter your cargo volume, \nthen determine whether you will need to use a containerized service or ship \nas less than container load"
-"\n"
-puts "Please let us know your cargo volume"
+end
 
-cargo_volume = gets.chomp.to_f
 
-puts calculate_containers_number(cargo_volume)
 
-puts "now please tell us what is the load port / origin of your goods"
+# puts "\n\nWelcome to the Melbourne Import FOB calculator"
+# puts "This calculator will allow you to enter your cargo volume, \nthen determine whether you will need to use a containerized service or ship \nas less than container load"
+# "\n"
+# puts "Please let us know your cargo volume"
 
-load_port = gets.chomp.capitalize
+# cargo_volume = gets.chomp.to_f
 
-origin_port(load_port, "./docs/pricing.csv")
+# puts calculate_containers_number(cargo_volume)
+
+
+# puts "now please tell us what is the load port / origin of your goods"
+
+# load_port = gets.chomp.capitalize
+
+# origin_port(load_port, "./docs/pricing.csv")
+
