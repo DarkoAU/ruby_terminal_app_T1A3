4088052912bc40f98a6d4bd25cae9c46eeb67388
YOUR_USERNAMEDarkoAU <GCAM022011@coderacademy.edu.au>
2020-12-20 18:03:35 +1100

    updated directory and files and folders

---
+++ src/shipping_calculator.rb
@@ -0,0 +1,40 @@
+require 'csv'
+require 'tty-prompt'
+require_relative 'methods'
+
+prompt = TTY::Prompt.new
+
+while true
+    sys_clear 
+    loop do
+        menu_select = prompt.select("Welcome to the Melbourne Import Calculator. Please select from one of the folllowing options: \n\n", ['Instruction to Use', 'Run the Shipping Calculator', 'Quit Application'])
+        case menu_select
+            when 'Instruction to Use'
+                how_to_use
+                sys_clear
+            when 'Run the Shipping Calculator'
+                shipping_calculator_running = true
+                while shipping_calculator_running
+                    sys_clear 
+                    puts "Please provide total cargo volume"
+                    cargo_volume = gets.chomp.to_f
+                    containers = calculate_containers_number(cargo_volume)
+                    puts "\n\nNow, please tell us your origin port"
+                    port = origin_port("./pricing/pricing.csv")
+                    cost = calculate_shipping_costs(containers, port)
+                    puts "\n\n\n                    *** The TOTAL FREIGHT COST for your shipment will be US$ #{cost} ***"
+                    puts "\n\n\n\nDo you want to do another caculation? Enter 1 for yes, Enter any other key to go back"
+                    shipping_calculator_choice = gets.chomp.to_i
+                    if shipping_calculator_choice == 1
+                        shipping_calculator_running = true
+                    else 
+                        shipping_calculator_running = false
+                        sys_clear
+                    end
+                end
+            when 'Quit Application'
+                sys_clear 
+                quit
+        end
+    end
+end
---
+++ src/spec/methods_spec.rb
@@ -0,0 +1,49 @@
+
+require_relative '../methods'
+
+describe "calculate_containers_number" do 
+    it 'should return LCL based on volume when volume is less than 15' do
+       containers_hash = calculate_containers_number(13)
+       expect(containers_hash[:LCL]).to eq(13)
+    end
+    it 'should return return twenty_foot when volume is between 15 and 25' do
+       containers_hash = calculate_containers_number(20)
+       expect(containers_hash[:twenty_foot]).to eq(1)
+    end
+    it 'should return return fourty_foot when volume is over 25 and 50' do
+        containers_hash = calculate_containers_number(30)
+        expect(containers_hash[:fourty_foot]).to eq(1)
+     end
+     it 'should return divide volume by number of containers when volume is over 50' do
+        containers_hash = calculate_containers_number(120)
+        expect(containers_hash[:twenty_foot]).to eq(1)
+        expect(containers_hash[:fourty_foot]).to eq(2)
+     end
+end
+
+describe "calculate_shipping_costs" do
+   it 'should return 161.00 for 2.3 cbm from Hamburg Port' do 
+      containers = calculate_containers_number(2.3) 
+      port = ['Hamburg',1850,3700,70]
+      total_cost = calculate_shipping_costs(containers, port)
+      expect(total_cost).to eq(161.00)
+   end
+   it 'should return 2350.00 for 24 cbm from Hong Kong Port' do 
+      containers = calculate_containers_number(24)
+      port = ['Hong Kong',2350,4700,56]
+      total_cost = calculate_shipping_costs(containers, port)
+      expect(total_cost).to eq(2350.00)
+   end
+   it 'should return 3500.00 for 43 cbm from Durban Port' do 
+      containers = calculate_containers_number(43)
+      port = ['Durban',1750,3500,94]
+      total_cost = calculate_shipping_costs(containers, port)
+      expect(total_cost).to eq(3500.00)
+   end
+   it 'should return 12250.00 for 120 cbm from Shanghai Port' do 
+      containers = calculate_containers_number(120)
+      port = ['Shanghai',2450,4900,61]
+      total_cost = calculate_shipping_costs(containers, port)
+      expect(total_cost).to eq(12250.00)
+   end
+end
